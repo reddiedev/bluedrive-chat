9b13e9e9e15d76d534080c65b33ba6010c2229b2
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-06-09 06:52:55 +0800

    ✨ (utils.ts): add isValidUUID function to validate UUID v4 format using regex

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isValidUUID(uuid: string) {
+  const UUID_V4_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+  return UUID_V4_REGEX.test(uuid)
+}
+
+
