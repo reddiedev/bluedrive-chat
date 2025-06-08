977eb9e8d14917da404826808a978a9192c71caf
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-06-08 14:10:57 +0800

    ✨ Update Docker Compose to include PostgreSQL service with environment variables and persistent storage. Enhance FastAPI backend to integrate PostgreSQL for chat message history management, and update dependencies in pyproject.toml and uv.lock.

--- backend/main.py
+++ backend/main.py
@@ -3,25 +3,45 @@ import uvicorn
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_ollama.llms import OllamaLLM
+from langchain_postgres import PostgresChatMessageHistory
+import psycopg
 import os
-
-system_prompt = (
-    """You are Qwen, created by Alibaba Cloud. You are a helpful assistant."""
-)
+import uuid
 
 
-model = OllamaLLM(
-    model=os.getenv("OLLAMA_MODEL"), base_url=os.getenv("OLLAMA_BASE_URL")
-)
+def generate_session_id():
+    return str(uuid.uuid4())
+
+
+def generate_message_id():
+    return str(uuid.uuid4())
 
 
 load_dotenv(override=True)
 
+system_prompt = (
+    """You are Qwen, created by Alibaba Cloud. You are a helpful assistant."""
+)
+model = OllamaLLM(
+    model=os.getenv("OLLAMA_MODEL"), base_url=os.getenv("OLLAMA_BASE_URL")
+)
+
+CONNECTION_STRING = (
+    f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}"
+    f":{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+)
+
+table_name = "bd_chat_history"
+sync_connection = psycopg.connect(CONNECTION_STRING)
+PostgresChatMessageHistory.create_tables(sync_connection, table_name)
+
 
 class ChatRequest(BaseModel):
-    question: str
+    name: str = "User"
+    session_id: str
+    content: str
 
 
 app = FastAPI()
@@ -37,20 +57,35 @@ async def health():
     return {"message": "OK"}
 
 
+sys_msg = SystemMessage(content=system_prompt)
+
+
 @app.post("/chat")
 async def chat(request: ChatRequest):
-    prompt = ChatPromptTemplate.from_messages(
-        [SystemMessage(content=system_prompt), HumanMessage(content=request.question)]
+    print(f"Received request: {request}")
+
+    chat_history = PostgresChatMessageHistory(
+        table_name, request.session_id, sync_connection=sync_connection
     )
+    prev_messages = chat_history.get_messages()
+    print(f"Previous messages: {prev_messages}")
+
+    new_usr_msg = HumanMessage(
+        content=request.content, id=generate_message_id(), name=request.name
+    )
+    prompt = ChatPromptTemplate.from_messages([sys_msg] + prev_messages + [new_usr_msg])
     chain = prompt | model
 
-    response = chain.invoke({"question": request.question})
-    print(response)
+    response = chain.invoke({"content": request.content})
+    new_ai_msg = AIMessage(content=response, id=generate_message_id(), name="Assistant")
+
+    chat_history.add_messages([new_usr_msg, new_ai_msg])
+    print(f"Response: {response}")
+
     return {"message": response}
 
 
 def main():
-    print("Starting server...")
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
