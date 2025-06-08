89b37bf32d93b3f2efe58bdced0806c2d14f8e24
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-06-08 14:41:13 +0800

    ✨ Enhance FastAPI backend with session management and title generation for chat sessions, implement database interactions for session storage, and update dependencies in pyproject.toml and uv.lock.

--- backend/main.py
+++ backend/main.py
@@ -1,47 +1,105 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
-from pydantic import BaseModel
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_ollama.llms import OllamaLLM
 from langchain_postgres import PostgresChatMessageHistory
 import psycopg
+from psycopg import Connection
 import os
-import uuid
 
-
-def generate_session_id():
-    return str(uuid.uuid4())
-
-
-def generate_message_id():
-    return str(uuid.uuid4())
+from lib.utils import generate_message_id, is_session_id_valid
+from lib.types import ChatRequest, Session
 
 
 load_dotenv(override=True)
 
+## LLM START
 system_prompt = (
     """You are Qwen, created by Alibaba Cloud. You are a helpful assistant."""
 )
+sys_msg = SystemMessage(content=system_prompt)
 model = OllamaLLM(
     model=os.getenv("OLLAMA_MODEL"), base_url=os.getenv("OLLAMA_BASE_URL")
 )
 
+
+def get_session_title(usr_msg: str) -> str:
+    sys_msg = SystemMessage(
+        content=f"""
+        {system_prompt}
+        The user will provide a message, and you will generate a title for the chat session.
+        The title should be a single word or phrase that captures the essence of the user's message.
+        The title should be in the same language as the user's message.
+        The title should start with an emoji.
+        The title should be no less than 10 words and no more than 100 characters.
+        """
+    )
+    prompt = ChatPromptTemplate.from_messages([sys_msg, HumanMessage(content=usr_msg)])
+    chain = prompt | model
+    response = chain.invoke({"content": usr_msg})
+    return response
+
+
+## LLM END
+
+
+## DATABASE START
+def create_db_sessions_table(conn: Connection):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS db_sessions (
+                id VARCHAR(255) PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            """
+        )
+        conn.commit()
+
+
+def get_session_by_id(conn: Connection, session_id: str) -> Session | None:
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT id, username, title FROM db_sessions WHERE id = %s
+            """,
+            (session_id,),
+        )
+        result = cur.fetchone()
+        if result:
+            return Session(id=result[0], title=result[2], username=result[1])
+        return None
+
+
+def create_session_if_not_exists(
+    conn: Connection, session_id: str, user_name: str, session_title: str
+):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO db_sessions (id, username, title)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (id) DO NOTHING
+            """,
+            (session_id, user_name, session_title),
+        )
+        conn.commit()
+
+
+table_name = "bd_chat_history"
 CONNECTION_STRING = (
     f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}"
     f":{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 )
-
-table_name = "bd_chat_history"
 sync_connection = psycopg.connect(CONNECTION_STRING)
 PostgresChatMessageHistory.create_tables(sync_connection, table_name)
+create_db_sessions_table(sync_connection)
 
-
-class ChatRequest(BaseModel):
-    name: str = "User"
-    session_id: str
-    content: str
+## DATABASE END
 
 
 app = FastAPI()
@@ -49,20 +107,28 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
 
 
 @app.get("/health")
 async def health():
-    return {"message": "OK"}
-
-
-sys_msg = SystemMessage(content=system_prompt)
+    return {"message": "OK!"}
 
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
     print(f"Received request: {request}")
+
+    if not is_session_id_valid(request.session_id):
+        raise HTTPException(status_code=400, detail="Invalid session ID")
+
+    session = get_session_by_id(sync_connection, request.session_id)
+    if not session:
+        title = get_session_title(request.content)
+        session = Session(id=request.session_id, title=title, username=request.name)
+        create_session_if_not_exists(
+            sync_connection, request.session_id, request.name, title
+        )
 
     chat_history = PostgresChatMessageHistory(
         table_name, request.session_id, sync_connection=sync_connection
@@ -82,7 +148,11 @@ async def chat(request: ChatRequest):
     chat_history.add_messages([new_usr_msg, new_ai_msg])
     print(f"Response: {response}")
 
-    return {"message": response}
+    return {
+        "message": response,
+        "session": session,
+        "messages": chat_history.get_messages(),
+    }
 
 
 def main():
