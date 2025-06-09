180bf6bf1bf602b1d05e5af5a6e8f5b16ce83fac
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-06-09 22:31:22 +0800

    ♻️ (main.py): refactor CORS middleware to allow all origins for broader access during development

--- backend/main.py
+++ backend/main.py
@@ -140,14 +140,10 @@ create_db_sessions_table(sync_connection)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
