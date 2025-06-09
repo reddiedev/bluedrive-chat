92a9265c74faa81bdf4ddb4d8d65783dda7814b2
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-06-10 02:57:28 +0800

    ✨ (docker): add .dockerignore file and update Dockerfile for improved build process and environment configuration

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -10,6 +10,17 @@ export default defineConfig({
       tsConfigPaths({
         projects: ['./tsconfig.json'],
       }),
+      {
+        name: 'report-hmr-ports',
+        configureServer: ({ config }) => {
+          const hmr = config.server.hmr
+          if (typeof hmr === 'object' && 'port' in hmr) {
+            console.log(
+              `\x1b[34mHMR\x1b[0m is listening to \x1b[32mhttp://localhost:${hmr.port}\x1b[0m`,
+            )
+          }
+        },
+      },
     ],
   },
   server: {
